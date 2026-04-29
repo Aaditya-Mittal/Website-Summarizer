ce04626c071029fb8d0c2ec92480e0ed933a9d55
Aaditya-Mittal <adty2704@gmail.com>
2026-04-29 22:19:53 +0530

    Scraper -> Crawler upgrade in process

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
+from urllib.parse import urlkoin, urlparse
 
 async def scrape_url(url: str):
     async with async_playwright() as p:
@@ -19,6 +20,22 @@ async def scrape_url(url: str):
         clean_text = soup.get_text(separator=" ", strip=True)
 
         return clean_text
+
+async def crawl_website(base_url: str, max_pages: int = 5):
+    visited_urls = set()
+    urls_to_visit = [base_url]
+    extracted_text = []
+
+    base_domain = urlparse(base_url).netloc
+
+    async with async_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = await browser.new_page()
+
+        while urls_to_visit and len(visited_urls) < max_pages:
+            pass
+
+        
 
 if __name__ == "__main__":
     print("This file contains a function to scrape a website.")
