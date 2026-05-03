4be3f08b670eb51f25d2dbb96518b308d130d52e
Aaditya-Mittal <adty2704@gmail.com>
2026-05-03 12:34:55 +0530

    Works but needs enhancement

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import sys
+import asyncio
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from scraper import crawl_website
 from agent import summarize_text
 
+# Force Windows to use the correct async event loop for Playwright
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = ["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials = True,
-    allow_methods =["*"],
+    allow_methods = ["*"],
     allow_headers = ["*"]
 )
 
@@ -30,3 +37,6 @@ async def summarize_endpoint(request: URLRequest):
         "url": request.url,
         "summary": summary
     }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
